67ed1f1858bde8c909709b0cc08491c26c00b646
Victor Balaban <victor.balaban@yahoo.com>
2017-12-13 23:34:40 +0200

    bugfix for backend

--- backend/src/service/index.js
+++ backend/src/service/index.js
@@ -1,10 +1,5 @@
-var express = require('express');
-var router = express.Router();
-var cinema = require('../service/').pub;
+const user = require('./user');
 
-router.get('/', pub.list);
-router.get('/:id', pub.findById);
-router.post('/', pub.create);
-router.update('/', pub.update);
-
-module.exports = router;
+module.exports = {
+  user,
+};
